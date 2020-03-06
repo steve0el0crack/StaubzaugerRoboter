9eaa075e4c148d2defe9a425493a3af3c8010af6
Esteban Castillo <escaob1@gmail.com>
2020-03-06 19:19:01 +0100

    Recursive numbering complete PROBLEM: Overlapping

--- taktik.py
+++ taktik.py
@@ -11,6 +11,11 @@ for reihe in range(0, place_num[0]):
 		
 world[random.randint(0, 3) - 1][random.randint(0, 3) - 1][":index"] = 0
 
+def checkallplaces(key, value):
+	for y in range(0, place_num[0]):
+                for x in range(0, place_num[1]):
+                        if  world[y][x][key] == value:
+                                return True 
 
 def setdisplay():
 	for y in range(0, place_num[0]):
@@ -38,32 +43,40 @@ def searchinworld(place):
 			if place == world[y][x]:
 				return x, y	
 
-def numbering(origin):
-	x = origin[":x"]
-	y = origin[":y"]
-	for i in [1, -1]:
-		coords = searchinworld(searchplace(":x", ":y", x + i, y))
-		if coords != None:
-			setindex(coords, 1)		
-	for i in [1, -1]:
-		coords = searchinworld(searchplace(":x", ":y", x, y + i))
-		if coords != None:
-			setindex(coords, 1)	
-
-print "%%%%%%%%%%%%%%%%%"
-
 def searchindex(value):
 	for reihe in world:
                 for place in reihe:
                         if place[":index"] == value:
                                   return place    
 
+
+
+def numbering(origins, layer):
+	recurcoords = []
+	for origin in origins:
+		x = origin[0]
+		y = origin[1]
+		for i in [1, -1]:
+			coords = searchinworld(searchplace(":x", ":y", x + i, y))
+			if coords != None:
+				setindex(coords, layer)
+				recurcoords.append(coords)		
+		for i in [1, -1]:
+			coords = searchinworld(searchplace(":x", ":y", x, y + i))
+			if coords != None:
+				setindex(coords, layer)	
+				recurcoords.append(coords)
+	if checkallplaces(":index", "") == True:
+		print "*****************"
+		setdisplay()
+		presentworld()
+		numbering(recurcoords, layer + 1) 
+
 presentworld()
-numbering(searchindex(0))
-setdisplay()
 
-print "---------------------------"
+numbering([searchinworld(searchindex(0))], 1)
 
+print "*****************"
 presentworld()
 
 
