2b08725271c8e15c2227c17d72e1bcdb476c7ea2
Esteban Castillo <escaob1@gmail.com>
2020-03-15 18:01:36 +0100

    Better render (Origin --> X / Null --> )

--- taktik.py
+++ taktik.py
@@ -3,8 +3,6 @@ import sys
 import os
 import time
 
-#MAXIMAL DIMENSIONS of the world in terminal -----------> 35 x 35
-
 x = int(sys.argv[1])
 y = int(sys.argv[2])
 place_num = [x, y]
@@ -13,28 +11,36 @@ world = []
 for reihe in range(0, place_num[0]):
         world.append([])
         for place in range(0, place_num[1]):
-                world[reihe].append({":x": place, ":y": reihe, ":H" : 0, ":index" : "", ":display" :  "-"})
-		
-world[random.randint(0, 3) - 1][random.randint(0, 3) - 1][":index"] = 0
-
+                world[reihe].append({":x": place, ":y": reihe, ":state" : "", ":distance" : "", ":display" :  ""})	#WORLD
+			
+def setorigin():
+	ocoords = {":x" : random.choice(range(x)), ":y" : random.choice(range(y))}
+	world[ocoords[":y"]][ocoords[":x"]][":state"] = "X"
+	world[ocoords[":y"]][ocoords[":x"]][":distance"] = 0
+	return ocoords
+ocoords = setorigin()
 
 def setdisplay():
-	for y in range(0, place_num[0]):
-                for x in range(0, place_num[1]):
-			if  world[y][x][":index"] != "":
-				world[y][x][":display"] = world[y][x][":index"]  
-def countfreeplaces(key, value):
+	for yi, reihe in enumerate(world):								#DISPLAY ----> RENDERING
+                for xi, place in enumerate(reihe):
+			if  place[":distance"] != "":
+				place[":display"] = place[":distance"]					#NORMAL PLACE ------> DISTANCE   
+			if  place[":distance"] == 0:
+				place[":display"] = place[":state"]						#ORIGIN -----> X 		
+
+
+def countplacesby(key, value):										#COUNT by {key : value}
 	counter = 0
-	for y in range(0, place_num[0]):
-                for x in range(0, place_num[1]):
+	for y in range(0, place_num[1]):
+                for x in range(0, place_num[0]):
 			if  world[y][x][key] == value:
         			counter += 1                     
 	return counter 	
 
 
 def presentworld():
-	for y in range(0, place_num[0]):
-        	for x in range(0, place_num[1]):
+	for y in range(0, place_num[1]):
+        	for x in range(0, place_num[0]):
 			todisplay = str(world[y][x][":display"])
 			sys.stdout.write(todisplay.ljust(4))
 		print ""
@@ -45,50 +51,46 @@ def searchplace(xkey, ykey, xval, yval):
 			if place[xkey] == xval and place[ykey] == yval:
 				  return place 
 def setindex(coords, value): 
-	world[coords[1]][coords[0]][":index"] = value
+	world[coords[1]][coords[0]][":distance"] = value
 	
-def searchinworld(place):
+def searchinworld(place):											#Search by STRUCT, return COORDS
 	for y in range(0, place_num[0]):
 		for x in range(0, place_num[1]):
 			if place == world[y][x]:
 				return x, y	
 
-def searchindex(value):
+def searchbydistance(value):												#Return STRUCT
 	for reihe in world:
                 for place in reihe:
-                        if place[":index"] == value:
+                        if place[":distance"] == value:
                                   return place    
 
 
 
-def numbering(origins, layer):
+def numbering(origins, maxdistance):
 	recurcoords = []
 	for origin in origins:
 		x = origin[0]
 		y = origin[1]
 		for i in [1, -1]:
 			coords = searchinworld(searchplace(":x", ":y", x + i, y))
-			if coords != None and searchplace(":x", ":y", x + i, y)[":index"] ==  "":
-				setindex(coords, layer)
+			if coords != None and searchplace(":x", ":y", x + i, y)[":distance"] ==  "":
+				setindex(coords, maxdistance)
 				recurcoords.append(coords)		
 		for i in [1, -1]:
 			coords = searchinworld(searchplace(":x", ":y", x, y + i))
-			if coords != None and searchplace(":x", ":y", x, y + i)[":index"] ==  "":
-				setindex(coords, layer)	
+			if coords != None and searchplace(":x", ":y", x, y + i)[":distance"] ==  "":
+				setindex(coords, maxdistance)	
 				recurcoords.append(coords)	
-	setdisplay()
-	presentworld()
+	setdisplay()												#SET WHAT TO RENDER
+	presentworld()												#RENDER
 	time.sleep(0.2)
 	os.system("clear")
-	if countfreeplaces(":index", "") > 0:
-		#print "*****************"	
-		#presentworld()
-		numbering(recurcoords, layer + 1) 
-	
-
-#presentworld()
-
-numbering([searchinworld(searchindex(0))], 1)
+	if countplacesby(":distance", "") > 0:
+		return numbering(recurcoords, maxdistance + 1) 
+	else:
+		return maxdistance, recurcoords
+print numbering([searchinworld(searchbydistance(0))], 1)
 
 presentworld()
 
