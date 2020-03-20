ea5a49bee3363b425521f5f6a1880e8378f30d4a
Esteban Castillo <escaob1@gmail.com>
2020-03-20 21:43:56 +0100

    Move() PROGRESS, numbering() must be more efficient

--- taktik.py
+++ taktik.py
@@ -1,3 +1,4 @@
+import functools
 import random
 import sys
 import os
@@ -15,7 +16,7 @@ for reihe in range(0, place_num[0]):
 			
 def setorigin():
 	ocoords = {":x" : random.choice(range(x)), ":y" : random.choice(range(y))}
-	world[ocoords[":y"]][ocoords[":x"]][":state"] = "X"
+	world[ocoords[":y"]][ocoords[":x"]][":state"] = u"\u001b[38;5;124mX" + u"\u001b[0m   "   
 	world[ocoords[":y"]][ocoords[":x"]][":distance"] = 0
 	return ocoords
 ocoords = setorigin()
@@ -50,55 +51,84 @@ def searchplacebyrelation(xkey, ykey, xval, yval):
 		for place in reihe:
 			if place[xkey] == xval and place[ykey] == yval:
 				  return place 
-def setindex(coords, value): 
-	world[coords[1]][coords[0]][":distance"] = value
+def setrelationincoords(coords, key, value): 
+	world[coords[1]][coords[0]][key] = value
 	
-def searchinworld(place):											#Search by STRUCT, return COORDS
+def getcoords(place):											#Search by STRUCT, return COORD
 	for y in range(0, place_num[0]):
 		for x in range(0, place_num[1]):
 			if place == world[y][x]:
 				return x, y	
+def getplace(coords):
+        return [place for yi, element in enumerate(world) for xi, place in enumerate(element) if (xi, yi) == coords]
 
-def searchbydistance(value):												#Return STRUCT
-	return [place for reihe in world for place in reihe if place[":distance"] == value]
+def searchplaceby(key, value):												#Return STRUCT
+	return [place for reihe in world for place in reihe if place[key] == value]
+
+def nebendecoords(origincoord):
+        nebendecoords = []
+       	for i in [1, -1]:
+                ynext = searchplacebyrelation(":x", ":y", origincoord[0], origincoord[1] + i)
+                xnext = searchplacebyrelation (":x", ":y", origincoord [0] + i, origincoord [1])
+                ycoord = getcoords(ynext)
+                xcoord = getcoords(xnext)
+                        
+                if ycoord != None and ynext[":distance"] ==  "":
+                        nebendecoords.append(ycoord)	
+                if xcoord != None and xnext[":distance"] ==  "":
+                        nebendecoords.append(xcoord)		
+
+        return nebendecoords
+
+TEST = []
 
 
 def numbering(origins, maxdistance):
-	recurcoords = []
-	for origin in origins:											#SETTING A ROUND OF DISTANCES
-		x = origin[0]
-		y = origin[1]
-		for i in [1, -1]:
-			coords = searchinworld(searchplacebyrelation(":x", ":y", x + i, y))
-			if coords != None and searchplacebyrelation(":x", ":y", x + i, y)[":distance"] ==  "":
-				setindex(coords, maxdistance)
-				recurcoords.append(coords)		
-		for i in [1, -1]:
-			coords = searchinworld(searchplacebyrelation(":x", ":y", x, y + i))
-			if coords != None and searchplacebyrelation(":x", ":y", x, y + i)[":distance"] ==  "":
-				setindex(coords, maxdistance)	
-				recurcoords.append(coords)	
-	setinitialdisplay()											#SET WHAT TO RENDER base on DISTANCES
-	presentworld()												#RENDER
-	time.sleep(0.2)
-	os.system("clear")
-	if countplacesby(":distance", "") > 0:
-		return numbering(recurcoords, maxdistance + 1) 
-	else:
-		return maxdistance, recurcoords
-print numbering([searchinworld(searchbydistance(0)[0])], 1)
-presentworld()
+        TEST.append (origins)
+        try:
+                for origin in origins:
+                        recurcoords = nebendecoords(origin)
+                        map(functools.partial(setrelationincoords, key=":distance", value=maxdistance), recurcoords)
+                setinitialdisplay()											#SET WHAT TO RENDER base on DISTANCES
+	        presentworld()												#RENDER
+	        time.sleep(1)
+	        os.system("clear")
+	        if countplacesby(":distance", "") > 0:
+		        return numbering(recurcoords, maxdistance + 1) 
+	        else:
+		        return maxdistance, recurcoords
+        except UnboundLocalError:
+                print TEST
+        
+def poprelationincoords(coords, key): 
+	return world[coords[1]][coords[0]].pop(key)
+        
+def move(ocoords, cond):                                             #RECURSIVE -----> ONLY one COORD NEEDED, ONLY 1 ELEMENT TO MOVE (X)    
 
-"""
-def move(ocoords, cond):                                                     #RECURSIVE -----> ONLY one COORD NEEDED, ONLY 1 ELEMENT TO MOVE (X)      
-        os.system("clear")
+        #SETING DESTINY
+        maxdistance, goalcoords = numbering([getcoords(searchplaceby(":distance", 0)[0])], 1)
 	presentworld()
 
-#RANDOM MOVEMENT ------> NIKITA
-	goalindex = searchinworld(random.choice(searchbydistance(1)))		
-	
+        #SETING START
+        start = getcoords(searchplaceby(":distance", 0)[0])
+        
+        
+	#RANDOM MOVEMENT ------> NIKITA
+        def randmovement(currentcoords):
+                inmediategoal = random.choice(map(getcoords, searchplaceby(":distance", getplace(currentcoords)[0][":distance"] + 1)))
+                setrelationincoords(inmediategoal, ":display", poprelationincoords(currentcoords, ":display"))
+                setrelationincoords(currentcoords, ":display", u"\u001b[38;5;2mX" + u"\u001b[0m   ")
+                os.system("clear")
+                presentworld()
+                time.sleep(1)
+                if getplace(currentcoords)[0] [":distance"] != maxdistance:
+                        return randmovement(inmediategoal)
+                else:
+                        return "ONE CYCLE"
+        randmovement(start)
 move(0, 0)
-	
+
+"""	
 	if coords[":y"] + change[1] == ydim - 1:                        #Y EDGE
 		nextcoords = {":x" : coords[":x"], ":y" : 0}
 
