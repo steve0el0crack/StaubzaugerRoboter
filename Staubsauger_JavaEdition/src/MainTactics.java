52c202053932b934d91d4ce1706e481267d03efe
misofflo <61591151+misofflo@users.noreply.github.com>
2020-03-20 19:45:17 +0100

    Java Version
    World generation and numbering complete.
    Visualization partially completed.

---
+++ Staubsauger_JavaEdition/src/MainTactics.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainTactics {
+    private static World world;
+    private static Random rng = new Random();
+
+    // main process
+    public static void main(String[] args) {
+        world = new World(10, 10);
+
+        setOrigin();
+
+        numbering(searchByDistance(0), 1);
+
+        world.present();
+
+        new Visualizer();
+    }
+
+    private static int[] setOrigin() {
+        world.fields.get(rng.nextInt(world.fields.size())).index = 0;
+        return world.fields.get(rng.nextInt(world.fields.size())).coords;
+    }
+
+    private static void setIndex(int[] coords, int value) {
+        for (int i = 0; i < world.fields.size(); i++) {
+            if (world.fields.get(i).coords == coords) {
+                world.fields.get(i).index = value;
+            }
+        }
+    }
+
+    private static int countPlaces(int pIndex) {
+        int counter = 0;
+        for (World.Field f : world.fields) {
+            if (f.index == pIndex) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static World.Field searchField(int x, int y) {
+        World.Field field = world.fields.get(0);
+
+        for (World.Field f : world.fields) {
+            if (f.coords[0] == x) {
+                if (f.coords[1] == y) {
+                    field = f;
+                }
+            }
+        }
+
+        return field;
+    }
+
+    private static int[] searchInWorld(World.Field field) {
+        int[] coords = new int[2];
+
+        for (World.Field f : world.fields) {
+            if (f == field) {
+                coords = f.coords;
+            }
+        }
+
+        return coords;
+    }
+
+    private static ArrayList<int[]> searchByDistance(int value) {
+        ArrayList<int[]> coords = new ArrayList<>();
+
+        for (World.Field f : world.fields) {
+            if (f.index == value) {
+                coords.add(f.coords);
+            }
+        }
+
+        return coords;
+    }
+
+    // recursive method
+    private static void numbering(ArrayList<int[]> origins, int layer) {
+        ArrayList<int[]> recurcoords = new ArrayList<>();
+
+        for (int[] origin : origins) {
+            int x = origin[0];
+            int y = origin[1];
+
+
+            for (int i = -1; i <= 1; i++) {
+                if (i == 0) i = 1;
+
+                // for x-values
+                int[] coords = searchInWorld(searchField(x + i, y));
+                if (coords != null && searchField(x + i, y).index == -1) {
+                    setIndex(coords, layer);
+                    recurcoords.add(coords);
+                }
+
+                // for y-values
+                coords = searchInWorld(searchField(x, y + i));
+                if (coords != null && searchField(x, y + i).index == -1) {
+                    setIndex(coords, layer);
+                    recurcoords.add(coords);
+                }
+            }
+        }
+
+        if (countPlaces(-1) > 0) {
+            numbering(recurcoords, layer+1);
+        }
+    }
+}
---
+++ Staubsauger_JavaEdition/src/World.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class World {
+    public ArrayList<Field> fields = new ArrayList<>();
+
+    final int width;
+    final int height;
+
+    public World(int x, int y) {
+        width = x;
+        height = y;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                fields.add(new Field(i, j, false, -1));
+            }
+        }
+    }
+
+    public void present() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                for (Field f : fields) {
+
+                    if (f.coords[0] == x && f.coords[1] == y) {
+                        System.out.print(f.index + "  ");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public class Field {
+        public int[] coords = new int[2];
+        public boolean blocked = false;
+        public int index;
+
+        Field(int pX, int pY, boolean pBlocked, int pIndex) {
+            coords[0] = pX;
+            coords[1] = pY;
+            blocked = pBlocked;
+            index = pIndex;
+        }
+    }
+}
