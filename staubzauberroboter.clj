8b597430978e31e9da65566e2d0818c52a34aa29
Esteban Castillo <escaob1@gmail.com>
2020-04-04 23:40:45 +0200

    REFS and AGENTS

--- staubzauberroboter.clj
+++ staubzauberroboter.clj
@@ -1,48 +1,95 @@
-(ns proyect1.staubzauberroboter
+(ns StaubzaugerRoboter.staubzauberroboter
   (:gen-class))
 
 ;;basic variables
 (def world-dims {:x 3 :y 3})
-
 ;;world created with all positive values
-(defstruct place :x :y :state :distance :display)
-(def initialworld (for [y (range (:y world-dims)) x (range (:x world-dims))] (struct place x y)))
+(def world
+  (apply vector (map (fn [y]
+                       (apply vector (map (fn [x]
+                                            (ref {}))
+                                          (range (:x world-dims)))))
+                     (range (:y world-dims)))))
 
-;;origin seted
-(def setorigin
-  (assoc
-   (nth initialworld (rand (+ (:y world-dims) 1)))
-   :state "X"
-   :distance 0))
+(defn get-place [[x y]]
+  (-> world
+      (nth y)
+      (nth x)))  ;;I will not dereference the atom here because I want the ATOM itself, and not the value
+
+(defn add-robot
+  [place robot]
+  (cond
+    (some (fn [key] (= :R key)) (keys @place)) "BESETZT"  ;;if there is already a robot there... may later be if an obstacle is present.
+    :else (dosync
+           (alter place assoc :R robot))))
+
+(defn create-robot
+  [ID]
+  (let [x (rand-int (:x world-dims))
+        y (rand-int (:y world-dims))]
+    (add-robot (get-place [x y]) (agent {ID [x y]}))
+    [x y]))
+
+(def robots (doall (map create-robot ["X" "Y"])))  ;;The problem here comes when more than one robot is assigned the same place. I must be able to manage it so, that only one gets to the actual place, and the rest waits until it is disoccupied.
+
+(map (fn [coord] (get-place coord)) robots)
+
+;;origin
+(def origin
+  (let [x (rand-int (:x world-dims))
+        y (rand-int (:y world-dims))]
+    (swap!
+     (get-place [x y] )
+     assoc :R "X" :distance {"X" 0})
+    [x y]))
+
 
 ;;to detect when it is origin, and not to touch it
-(defn is-start?? [place]
+(defn is-start? [place]
   (if (and (= (:x place) (:x setorigin)) (= (:y place) (:y setorigin)))
-    place)
-  )
+    place))
+
+(defn get-places-around [[x y]]
+  (let [xindex (- (:x world-dims) 1)
+        yindex (- (:y world-dims) 1)]
+    (remove (fn [coord] (or (> (first coord) xindex)
+                            (> (second coord) yindex)
+                            (some (fn [axis] (< axis 0)) coord)))
+            (map (fn [delta]
+                   (let [dx (first delta)
+                         dy (second delta)]
+                     [(+ x dx) (+ y dy)]))
+                 [[1 0]
+                  [-1 0]
+                  [0 1]
+                  [0 -1]]))))
 
 ;;since every index is positive, and there are no diagonal movements. Its just a matter of math.
 (defn number-places-around
   [start  
-   universe
    distance]
-  (map (fn [place] 
-         (let [x-distance (Math/abs (- (:x place) (:x start)))
-               y-distance (Math/abs (- (:y place) (:y start)))
-               total-distance (+ x-distance y-distance)]
-           (cond 
-             (not (= (:distance place) nil)) place
-             (= 1 total-distance) (assoc place :distance distance)
-             (= 0 total-distance) start
-             true place)))
-       universe))
+  (map (fn [coord]
+         (let [place (get-place coord)]
+           (when-not (some (fn [a] (= a "X")) (keys (:distance @place)))
+             (swap! place update :distance assoc "X" distance))))
+       (get-places-around start)))
+
+(number-places-around origin 1)
+(get-places-around origin)
+(get-place origin)
+world
 
 ;;conditions used while numbering
-(defn all-numbered?
-  [universe]
-  (if (= (count (filter (fn [place] (= (:distance place) nil)) universe)) 0)
+(defn all-numbered-by?
+  [ID]
+  (if (= (count (remove
+                 (fn [atom] (some (fn [key] (= key ID))
+                                  (keys (:distance @atom))))
+                 world)) 0)
     true
     false))
+
+
 
 (defn get-numbered
   [universe
@@ -81,4 +128,13 @@
 (def numbered-world (numbering world))
 
 
+(def a (atom []))
 
+(pmap
+ (fn [x]
+   (swap! a conj x))
+ (range 10))
+
+(conj [] 1)
+
+(shutdown-agents)
