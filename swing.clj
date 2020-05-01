5786bf7aab21cdc5ab73b8df99945b19c0b906cd
Esteban Castillo <escaob1@gmail.com>
2020-05-01 18:39:05 +0200

    Following https://bjoernstechblog.rueffer.info/posts/lisp/java/swing/clojure/2010/12/28/A-simple-example-of-a-SWING-GUI-with-clojure./ code, I capture the java.awt.event.ActionEvent after pressing the button in an atom with the objective of inspecting it

--- swing.clj
+++ swing.clj
@@ -2,13 +2,27 @@
 
 (import
  '(java.awt Color Graphics Dimension)
- '(javax.swing JPanel JFrame))
+ '(javax.swing JPanel JFrame JButton JLabel)
+ '(java.awt.event ActionListener))
 
-(def frame (doto (new JFrame) (.add
-                               (doto (proxy [JPanel] [])
-                                 (.setPreferredSize (new Dimension
-                                                         300
-                                                         300))))
-                 .pack .show))
+(def catcher (atom nil))
+
+
+(def frame (doto (new JFrame)
+             (.add
+              (doto (new JPanel)
+                (.add (JLabel. "Hallo"))  ;;se agrega un JLabel al JPanel; lo que podria igualarse a un appendChild
+                (.add
+                 (doto (new JButton)
+                   (.addActionListener
+                    (proxy [java.awt.event.ActionListener] []
+                      (actionPerformed [e] (swap! catcher (fn [_] e)))))))
+                (.setPreferredSize (new Dimension
+                                        300
+                                        300))))
+             .pack
+             .show))
+
+@catcher
 
 
